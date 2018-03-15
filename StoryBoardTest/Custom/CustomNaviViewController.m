3cfd5d165d536ca845422b56b65cb0ce90ed6063
elear218 <eall@ealldemacbook-pro.local>
2018-03-15 12:05:26 +0800

    完善导航条

--- StoryBoardTest/Custom/CustomNaviViewController.m
+++ StoryBoardTest/Custom/CustomNaviViewController.m
@@ -8,7 +8,7 @@
 
 #import "CustomNaviViewController.h"
 
-@interface CustomNaviViewController ()
+@interface CustomNaviViewController ()<UIGestureRecognizerDelegate>
 
 @end
 
@@ -18,15 +18,41 @@
     [super viewDidLoad];
     
     self.navigationBar.translucent = NO;
-    // Do any additional setup after loading the view.
+    //导航条前景色
+//    [self.navigationBar setBarTintColor:[UIColor redColor]];
+    
+    //左右按钮颜色
+    [self.navigationBar setTintColor:[UIColor blackColor]];
+    
+    //导航栏文字属性
+//    NSMutableDictionary *barAttrs = [NSMutableDictionary dictionary];
+//    [barAttrs setObject:[UIColor blackColor] forKey:NSForegroundColorAttributeName];
+//    [barAttrs setObject:[UIFont systemFontOfSize:16.f] forKey:NSFontAttributeName];
+//    [self.navigationBar setTitleTextAttributes:barAttrs];
+    
+    // 手势代理
+    self.interactivePopGestureRecognizer.delegate = self;
 }
 
 - (void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated{
     
     if (self.viewControllers.count) {
         [viewController setHidesBottomBarWhenPushed:YES];
+        //设置导航栏返回按钮
+        UIBarButtonItem *back = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"back_white"] style:UIBarButtonItemStylePlain target:self action:@selector(popViewControllerAnimated:)];
+        viewController.navigationItem.leftBarButtonItem = back;
     }
     [super pushViewController:viewController animated:animated];
+}
+
+#pragma mark - <UIGestureRecognizerDelegate>
+/**
+ 决定是否触发手势
+ */
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldReceiveTouch:(UITouch *)touch
+{
+    //排除根控制器，其他所有子控制器都要触发手势
+    return self.viewControllers.count > 1;
 }
 
 - (void)didReceiveMemoryWarning {
